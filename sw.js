035845c0cf86ba8607649d975c8ad617539ad156
JonaQuiro <94380177+JonaQuiro@users.noreply.github.com>
2025-12-30 10:44:18 -0300

    Add files via upload
    v-2

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "CosmeShow";
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./style/style.css",
+    "./js/script.js",
+    "./js/cartas.js",
+    "./img/icon-192.png",
+    "./img/icon-512.png"
+];
+
+// INSTALL
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+// ACTIVATE
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+// FETCH
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
